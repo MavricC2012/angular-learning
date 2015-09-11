0e286876f2908762e2d8140a4e5d9114c3bd7201
Craig Weysham <craig@housingmanager.com>
2015-09-11 16:55:34 -0500

    Finished module 5

--- server/config/express.js
+++ server/config/express.js
@@ -14,7 +14,8 @@ module.exports = function(app, config) {
     app.set('views', config.rootPath + '/server/views');
     app.set('view engine', 'jade');
     app.use(logger('dev'));
-    app.use(cookieParser());
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(bodyParser.json());
     app.use(bodyParser());
     app.use(session({secret: 'multi vision unicorns'}));
     app.use(passport.initialize());
