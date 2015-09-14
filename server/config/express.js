a29b532a9333c1f137571d33dcb3f3b552a42670
Craig Weysham <craig@housingmanager.com>
2015-09-14 12:20:52 -0500

    Testing auth

--- server/config/express.js
+++ server/config/express.js
@@ -6,6 +6,7 @@ var express = require('express'),
     session = require('express-session'),
     passport = require('passport');
 
+
 module.exports = function(app, config) {
     function compile(str, path) {
         return stylus(str).set('filename', path);
@@ -14,8 +15,7 @@ module.exports = function(app, config) {
     app.set('views', config.rootPath + '/server/views');
     app.set('view engine', 'jade');
     app.use(logger('dev'));
-    app.use(bodyParser.urlencoded({extended: true}));
-    app.use(bodyParser.json());
+    app.use(cookieParser());
     app.use(bodyParser());
     app.use(session({secret: 'multi vision unicorns'}));
     app.use(passport.initialize());
