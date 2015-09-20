a4110021d2642fff285fbb407ed48da5cee174f3
Craig Weysham <battlemaster2012@gmail.com>
2015-09-20 11:51:31 -0500

    modified the file

--- public/app/app.js
+++ public/app/app.js
@@ -7,7 +7,7 @@ angular.module('app').config(function($routeProvider, $locationProvider) {
     }}
   }
 
-  $locationProvider.html5Mode(true);
+  $locationProvider.html5Mode({enabled:true, requireBase:false});
   $routeProvider
     .when('/', { templateUrl: '/partials/main/main', controller: 'mvMainCtrl'})
     .when('/admin/users', { templateUrl: '/partials/admin/user-list',
