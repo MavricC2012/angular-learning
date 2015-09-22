e1db64aaf60925fc390ec96a7cae30fe696b0680
MavricC2012 <battlemaster2012@gmail.com>
2015-09-21 19:17:18 -0500

    Update mvNotifier.js

--- public/app/common/mvNotifier.js
+++ public/app/common/mvNotifier.js
@@ -3,7 +3,7 @@ angular.module('app').value('mvToastr', toastr);
 angular.module('app').factory('mvNotifier', function(mvToastr) {
   return {
     notify: function(msg) {
-      mvToastr.success(msg);
+      mvToastr.success(msg{timeout:1000});
       console.log(msg);
     }
   }
